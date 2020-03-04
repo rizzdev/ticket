9292d31977118601a6a8cc260439c477508f3459
Andrew Rayner <arayner@netactuate.com>
2020-03-04 14:14:24 -0500

    uo

---
+++ src/config/data.js
@@ -0,0 +1,383 @@
+export default  [
+    {
+        name: 'Infractions',
+        charges: [
+            {
+                'name': 'Animal Cruelty',
+                'fine': 250,
+                'months': 2,
+                'restitution': null
+            },
+            {
+                'name': 'Disobeying an officer',
+                'fine': 500,
+                'months': 5,
+                'restitution': null
+            },
+            {
+                'name': 'Disturbing the Peace',
+                'fine': 65,
+                'months': 5,
+                'restitution': null
+            },
+            {
+                'name': 'Failure to Identify',
+                'fine': 250,
+                'months': 2,
+                'restitution': null
+            },
+            {
+                'name': 'Loitering',
+                'fine': 35,
+                'months': 2,
+                'restitution': null
+            },
+            {
+                'name': 'Public Intoxication (PC)',
+                'fine': 125,
+                'months': 3,
+                'restitution': null
+            },
+            {
+                'name': 'Failure to headlight',
+                'fine': 190,
+                'months': 2,
+                'restitution': null
+            },
+            {
+                'name': 'Failure to obey traffic control devices',
+                'fine': 150,
+                'months': 3,
+                'restitution': null
+            },
+            {
+                'name': 'Failure to yield to emergency vehicle',
+                'fine': 250,
+                'months': 3,
+                'restitution': null
+            },
+            {
+                'name': 'Illegal Parking',
+                'fine': 65,
+                'months': 2,
+                'restitution': null
+            },
+            {
+                'name': 'Noise Disturbance',
+                'fine': 125,
+                'months': 2,
+                'restitution': null
+            },
+            {
+                'name': 'Non Roadworthy Vehicle',
+                'fine': 65,
+                'months': 2,
+                'restitution': null
+            },
+            {
+                'name': 'Offroading (PC)',
+                'fine': 125,
+                'months': 2,
+                'restitution': null
+            },
+            {
+                'name': 'Speeding 10-19',
+                'fine': 315,
+                'months': 2,
+                'restitution': null
+            },
+        ],
+    },
+    {
+        name: 'Misdemeanor',
+        charges: [
+            {
+                'name': 'Assault or Battery',
+                'fine': 750,
+                'months': 8,
+                'restitution': null
+            },
+            {
+                'name': 'Attempted Bribery',
+                'fine': 1250,
+                'months': 6,
+                'restitution': null
+            },
+            {
+                'name': 'Brandishing a Weapon',
+                'fine': 250,
+                'months': 4,
+                'restitution': null
+            },
+            {
+                'name': 'Destruction of property',
+                'fine': 625,
+                'months': 5,
+                'restitution': null
+            },
+            {
+                'name': 'Drug Distribution',
+                'fine': 1250,
+                'months': 6,
+                'restitution': null
+            },
+            {
+                'name': 'Evidence Tampering',
+                'fine': 2000,
+                'months': 10,
+                'restitution': null
+            },
+            {
+                'name': 'Inciting a Riot',
+                'fine': 2500,
+                'months': 8,
+                'restitution': null
+            },
+            {
+                'name': 'Making a false report',
+                'fine': 625,
+                'months': 5,
+                'restitution': null
+            },
+            {
+                'name': 'Manslaughter',
+                'fine': 1875,
+                'months': 10,
+                'restitution': null
+            },
+            {
+                'name': 'Obstruction of justice',
+                'fine': null,
+                'months': 10,
+                'restitution': null
+            },
+            {
+                'name': 'Possession of Drug Paraphernalia',
+                'fine': 500,
+                'months': 5,
+                'restitution': null
+            },
+            {
+                'name': 'Possession of Controlled Substance',
+                'fine': 2000,
+                'months': 12,
+                'restitution': null
+            },
+            {
+                'name': 'Possession of Illegal Equiptment',
+                'fine': 500,
+                'months': 5,
+                'restitution': null
+            },
+            {
+                'name': 'Possession of illegal firearm',
+                'fine': 1500,
+                'months': 8,
+                'restitution': null
+            },
+            {
+                'name': 'Possession of Illegal weapon',
+                'fine': 875,
+                'months': 6,
+                'restitution': null
+            },
+            {
+                'name': 'Possession/Solicitation',
+                'fine': 750,
+                'months': 8,
+                'restitution': null
+            },
+            {
+                'name': 'Resisting Arrest (foot evasion)',
+                'fine': 1250,
+                'months': 6,
+                'restitution': null
+            },
+            {
+                'name': 'Theft',
+                'fine': 1250,
+                'months': 6,
+                'restitution': null
+            },
+            {
+                'name': 'Unlawful Discharge',
+                'fine': 500,
+                'months': 8,
+                'restitution': null
+            },
+            {
+                'name': 'Driving w/o license',
+                'fine': null,
+                'months': 10,
+                'restitution': null
+            },
+            {
+                'name': 'Reckless Driving',
+                'fine': 500,
+                'months': 6,
+                'restitution': null
+            },
+            {
+                'name': 'Speeding',
+                'fine': 1250,
+                'months': 8,
+                'restitution': null
+            },
+            {
+                'name': 'Street Racing',
+                'fine': 1500,
+                'months': 8,
+                'restitution': null
+            },
+            {
+                'name': 'Vehicular Manslaughter',
+                'fine': 1625,
+                'months': 10,
+                'restitution': null
+            },
+        ]
+    },
+    {
+        name: 'Felonies',
+        charges: [
+            {
+                'name': 'Accessory',
+                'fine': 400,
+                'months': 18,
+                'restitution': 2000
+            },
+            {
+                'name': 'Aiding/Escape Custody',
+                'fine': null,
+                'months': 20,
+                'restitution': null
+            },
+            {
+                'name': 'Armed Robbery',
+                'fine': 5000,
+                'months': 20,
+                'restitution': 2500
+            },
+            {
+                'name': 'Assault with a deadly',
+                'fine': 2500,
+                'months': 10,
+                'restitution': 1250
+            },
+            {
+                'name': 'Attempted Murder',
+                'fine': null,
+                'months': 20,
+                'restitution': 2500
+            },
+            {
+                'name': 'Drug Trafficking',
+                'fine': 2000,
+                'months': 10,
+                'restitution': null
+            },
+            {
+                'name': 'False Imprisonment',
+                'fine': 3750,
+                'months': 15,
+                'restitution': 1875
+            },
+            {
+                'name': 'Kidnapping',
+                'fine': null,
+                'months': 20,
+                'restitution': 2125
+            },
+            {
+                'name': 'Money Laundering',
+                'fine': 2000,
+                'months': 10,
+                'restitution': null
+            },
+            {
+                'name': 'Murder',
+                'fine': null,
+                'months': 30,
+                'restitution': 2500
+            },
+            {
+                'name': 'Robbery',
+                'fine': 4000,
+                'months': 16,
+                'restitution': 2500
+            },
+            {
+                'name': 'DUI',
+                'fine': null,
+                'months': 8,
+                'restitution': 500
+            },
+            {
+                'name': 'Fel. Evasion',
+                'fine': null,
+                'months': 10,
+                'restitution': 1250
+            },
+            {
+                'name': 'Grand Theft Auto',
+                'fine': null,
+                'months': 5,
+                'restitution': 1250
+            },
+            {
+                'name': 'Fel. Speeding 40+',
+                'fine': 3000,
+                'months': 12,
+                'restitution': 1500
+            },
+            {
+                'name': 'Hit and Run',
+                'fine': null,
+                'months': 12,
+                'restitution': 1500
+            },
+        ]
+    },
+    {
+        name: 'Aggravated',
+        charges: [
+            {
+                'name': 'Aggravated Assault',
+                'fine': 3750,
+                'months': 15,
+                'restitution': 1875
+            },
+            {
+                'name': 'Aggravated Kidnapping',
+                'fine': null,
+                'months': 30,
+                'restitution': 3200
+            },
+            {
+                'name': 'Aggravated Robbery',
+                'fine': 8750,
+                'months': 35,
+                'restitution': 3750
+            },
+            {
+                'name': 'Attempted Murder',
+                'fine': null,
+                'months': 35,
+                'restitution': 3750
+            },
+            {
+                'name': 'Aggravated Murder',
+                'fine': null,
+                'months': 35,
+                'restitution': 3750
+            },
+            {
+                'name': 'Aggravated Unlawful use of explosives',
+                'fine': null,
+                'months': 35,
+                'restitution': 3750
+            }
+        ]
+    }
+]
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,132 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <b-container>
+        <b-row>
+            <b-col md="6">
+                <div class="charge-section" v-for="(section, sectionIndex) in sections" :key="section.name">
+                    <b>{{ section.name }}</b>
+                    <div class="charge-subsection" v-for="(charge, chargeIndex) in section.charges" :key="charge.name">
+                        <b-row class="charge-item">
+                            <b-col md="7" style="cursor: pointer" @click="selectCharge(sectionIndex, chargeIndex)">
+                                <span v-if="charge.selected" class="text-primary font-weight-bold">{{ charge.name }}</span>
+                                <span v-else>{{ charge.name }}</span>
+                            </b-col>
+                            <b-col md="2">
+                                <span v-if="charge.fine">${{ charge.fine.toLocaleString() }}</span>
+                            </b-col>
+                            <b-col md="1">
+                                <span v-if="charge.months">{{ charge.months }}m</span>
+                            </b-col>
+                            <b-col md="2">
+                                <span v-if="charge.restitution">${{ charge.restitution.toLocaleString() }}</span>
+                            </b-col>
+                        </b-row>
+                    </div>
+                </div>
+            </b-col>
+            <b-col>
+                <b-row>
+                    <b-col style="margin-bottom: 25px">
+                        <button class="btn btn-block btn-outline-primary" @click="reset">
+                            Reset Charges
+                        </button>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col>
+                        <div class="font-weight-bold">If they want to pay fines</div>
+                        <div>
+                            <span v-if="sentence.fines <= 15000">Fines: <span class="text-primary">${{ sentence.fines.toLocaleString() }}</span></span>
+                            <span v-else>Fines: <span class="text-primary">${{ sentence.fines.toLocaleString() }}</span></span>
+                        </div>
+                        <div>
+                            <span v-if="sentence.man <= 45">(M) Time: <span class="text-primary">{{ sentence.man }} months</span></span>
+                            <span v-else>(M) Time: <span class="text-primary"><strike>{{ sentence.man }} months</strike> 45 months</span></span>
+                        </div>
+                    </b-col>
+                    <b-col>
+                        <div class="font-weight-bold">Don't want to pay fines</div>
+                        <div>
+                            <span v-if="sentence.time <= 45">Time: <span class="text-primary">{{ sentence.time }} months</span></span>
+                            <span v-else>Time: <span class="text-primary"><strike>{{ sentence.time }} months</strike> 45 months</span></span>
+                        </div>
+                        <div>
+                            <span v-if="sentence.res <= 10000">Restitution: <span class="text-primary">${{ sentence.res.toLocaleString() }}</span></span>
+                            <span v-else>Restitution: <span class="text-primary">${{ sentence.res.toLocaleString() }}</span></span>
+                        </div>
+                    </b-col>
+                </b-row>
+                <b-row style="margin-top: 25px">
+                    <b-col>
+                        <b>Charge List</b>
+                        <div>
+                            <span v-for="charge in sentence.charges">
+                                {{ charge.name }} |
+                            </span>
+                        </div>
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
+  import data from '../config/data';
+  import Vue from 'vue'
+  export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data() {
+    return {
+      sections: data
+    };
+  },
+  mounted() {
+
+  },
+  methods: {
+    reset() {
+      for(const sIndex in this.sections) {
+        for (const cIndex in this.sections[sIndex]['charges']) {
+          const charge = this.sections[sIndex]['charges'][cIndex];
+          if (charge.selected) {
+            Vue.set(this.sections[sIndex]['charges'][cIndex], 'selected', false);
+          }
+        }
+      }
+    },
+    selectCharge(sIndex, cIndex) {
+      Vue.set(this.sections[sIndex]['charges'][cIndex], 'selected', !this.sections[sIndex]['charges'][cIndex].selected);
+    },
+  },
+    computed: {
+      sentence() {
+        let data = {fines: 0, time: 0, res: 0, man: 0, charges: []};
+        for(const section of this.sections) {
+          for (const charge of section.charges) {
+            if (charge.selected) {
+              data.charges.push(charge);
+              if(charge.fine) data.fines += charge.fine;
+              if(charge.months) data.time += charge.months;
+              if(charge.restitution) data.res += charge.restitution;
+              if(!charge.fine && charge.months) data.man += charge.months;
+            }
+          }
+        }
+        return data;
+      }
+    }
 }
 </script>
+
+<style lang="scss">
+    .charge-subsection {
+        padding-left: 20px;
+    }
+    .charge-item {
+        // display: inline;
+        &:hover {
+            background-color: #d7d7d7;
+        }
+    }
+</style>
